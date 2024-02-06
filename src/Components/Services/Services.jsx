4b2473bf030a3f632b8bb85dcfbf8015bd4c5a9c
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-02-06 15:10:26 +0300

    Nike Website Clone

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,7 +1,12 @@
-
+import { services } from '../Constants'
+import ServiceCard from './ServiceCard'
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service}/> 
+      ))}
+    </section>
   )
 }
 
